32951b002f5d5ef73d5855f5bc75de43360e11e6
ygtsmt <ygtsmt@outlook.com>
2022-12-19 07:48:39 +0300

    provider_api

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => CommentProvider(),
       child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: HomePage(),
       ),
     );
